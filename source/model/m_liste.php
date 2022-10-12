e94fcc3903f6b3625ba8051643717ece4f96b9a0
LucasCoudrais <lucas.coudrais@gmail.com>
2022-10-12 14:58:40 +0200

    select recherchable avec vleur de la base de données

--- source/model/m_liste.php
+++ source/model/m_liste.php
@@ -25,10 +25,18 @@
     inner join symptpatho on patho.idp = symptpatho.idp
     inner join symptome on symptpatho.ids = symptome.ids
     inner join keySympt on symptome.ids = keySympt.ids
-    inner join keywords on keySympt.idk = keywords.idk
-    where keywords.name like '%pied%'");
+    inner join keywords on keySympt.idk = keywords.idk");
 		$query1->execute();
         $resultat1 = $query1->fetchAll(PDO::FETCH_ASSOC);
+
+    $query2 = $bdd->prepare("SELECT distinct name FROM public.keywords");
+    $query2->execute();
+        $resultat2 = $query2->fetchAll(PDO::FETCH_ASSOC);
+
+
+    $query3 = $bdd->prepare("SELECT distinct mer FROM public.patho");
+    $query3->execute();
+        $resultat3 = $query3->fetchAll(PDO::FETCH_ASSOC);
 	}	
 	catch(PDOException $e)
 	{
